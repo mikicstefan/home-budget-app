f11b08724fcb2f5e4b98c0ed1a15d0bf1a44774f
Stefan Mikic <stefan.mikic@codeus.me>
2022-01-29 12:16:51 +0100

    Define relationships

--- app/Models/User.php
+++ app/Models/User.php
@@ -41,4 +41,14 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    /**
+     * Returns all accounts for user
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function accounts()
+    {
+        return $this->hasMany(Account::class);
+    }
 }
